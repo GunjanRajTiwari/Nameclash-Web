6f1d1d2db26d18aee3b1f5cbc8b9127054f8c3e9
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-05-06 13:41:15 +0545

    production setup

--- js/chat.js
+++ js/chat.js
@@ -1,6 +1,6 @@
 // Make socket connection
-const socket =
-    io.connect("http://localhost:8000/") || io.connect("https://namesclash.herokuapp.com/");
+// const socket = io.connect("http://localhost:8000/");
+const socket = io.connect("https://namesclash.herokuapp.com/");
 
 const user = prompt("Enter your name:", "Anonymous");
 
